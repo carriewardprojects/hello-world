56fb10364f72ad337161e8726152556b77120ebe
Carrie Ward <carriewardprojects@gmail.com>
2017-03-03 22:56:11 -0800

    Java source code
    Hello World

---
+++ HelloWorldJava.java
@@ -0,0 +1,10 @@
+
+public class HelloWorldJava {
+
+	public static void main(String[] args) {
+		//a simple output statement
+		System.out.println("Hello World! This is a Java Program!");
+
+	}
+
+}
